497a510b9ee6780ea3291e9939d417c07d48ef85
Jon Reid <jon.reid@mac.com>
2012-11-29 19:53:53 -0800

    Add more to comment block

--- exportenv.sh
+++ exportenv.sh
@@ -1,5 +1,7 @@
 #
 #   Copyright 2012 Jonathan M. Reid. See LICENSE.txt
+#   Created by: Jon Reid, http://qualitycoding.org/
+#   Source: https://github.com/jonreid/XcodeCoverage
 #
 
 export | egrep '( BUILT_PRODUCTS_DIR)|(NATIVE_ARCH=)|(OBJECT_FILE_DIR_normal)|(SRCROOT)' > XcodeCoverage/env.sh
