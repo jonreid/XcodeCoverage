31a698c6aac8463d9823835250a9c9076f3cf7c5
Jon Reid <jon.reid@mac.com>
2012-11-29 20:21:51 -0800

    Didn't mean to check in env.sh

--- env.sh
+++
@@ -1,4 +0,0 @@
-export BUILT_PRODUCTS_DIR="/Users/joreid/Library/Developer/Xcode/DerivedData/CoverageSample-fjwuxzlwgygwlrazzlcibcpzgmar/Build/Products/Debug-iphonesimulator"
-export NATIVE_ARCH="i386"
-export OBJECT_FILE_DIR_normal="/Users/joreid/Library/Developer/Xcode/DerivedData/CoverageSample-fjwuxzlwgygwlrazzlcibcpzgmar/Build/Intermediates/CoverageSample.build/Debug-iphonesimulator/CoverageSample.build/Objects-normal"
-export SRCROOT="/Users/joreid/Desktop/CoverageSample"
