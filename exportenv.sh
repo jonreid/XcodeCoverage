10a96dd655266112143138579be73af4d2206b7c
endersstocker <bryan.endersstocker@me.com>
2014-03-24 07:57:45 -0400

    Update exportenv.sh

--- exportenv.sh
+++ exportenv.sh
@@ -5,4 +5,4 @@
 #
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export | egrep '( BUILT_PRODUCTS_DIR)|(CURRENT_ARCH)|(OBJECT_FILE_DIR_normal)|(SRCROOT)|(OBJROOT)' > "${DIR}"/env.sh
+export | egrep '( BUILT_PRODUCTS_DIR)|(CURRENT_ARCH)|(OBJECT_FILE_DIR_normal)|(SRCROOT)|(OBJROOT)' > "${DIR}/env.sh"
