9024b266a408937914eb7c05ed8ac95f930dbce4
Jon Reid <jon.reid@mac.com>
2014-06-10 22:01:15 -0700

    Fix Chinese locale dialog by forcing button text to be English
    https://github.com/jonreid/XcodeCoverage/pull/20

--- run_code_coverage_post.sh
+++ run_code_coverage_post.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-x=`/usr/bin/osascript <<EOT
+button=`/usr/bin/osascript <<EOT
 tell application "Finder"
   activate
-  set myReply to button returned of (display dialog "Generate code coverage report?")
+  set dialogText to "Generate code coverage report?"
+  set cancelText to "Cancel"
+  set okText to "OK"
+  set myReply to button returned of (display dialog dialogText buttons {cancelText, okText} cancel button cancelText default button okText)
 end tell
 EOT`
-if [[ $x = "OK" ]]; then 
-  echo 'Generating code coverage report'
+if [[ $button = "OK" ]]; then 
+  echo "Generating code coverage report"
   ${SRCROOT}/XcodeCoverage/getcov
 fi
-echo 'Done.'
+echo "Done."
