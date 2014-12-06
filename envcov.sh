7a7ae436bbb61b252247f6b2d0292bb6cf140ba7
Jon Reid <jon.reid@mac.com>
2014-12-06 13:56:18 -0800

    Streamline file header comments

--- envcov.sh
+++ envcov.sh
@@ -1,8 +1,5 @@
-#
+#   XcodeCoverage by Jon Reid, http://qualitycoding/about/
 #   Copyright 2014 Jonathan M. Reid. See LICENSE.txt
-#   Created by: Jon Reid, http://qualitycoding.org/
-#   Source: https://github.com/jonreid/XcodeCoverage
-#
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${DIR}/env.sh
