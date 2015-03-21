cbe7595a86a6eec2f911f039290705ed20d1baee
Jon Reid <jon.reid@mac.com>
2015-03-21 14:10:17 -0700

    llvm-cov-wrapper.sh: Lean toward Google shell style guide

--- llvm-cov-wrapper.sh
+++ llvm-cov-wrapper.sh
@@ -3,8 +3,8 @@
 #   Copyright 2014 Jonathan M. Reid. See LICENSE.txt
 
 if [ "$1" = "-v" ]; then
-    echo "llvm-cov-wrapper 4.2.1"
-    exit 0
+  echo "llvm-cov-wrapper 4.2.1"
+  exit 0
 else
-    /usr/bin/gcov "$@"
+  /usr/bin/gcov "$@"
 fi
