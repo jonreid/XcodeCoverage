9433076b7a9c7fd37c800939bad0cb0d2ab742e1
Jon Reid <jon.reid@mac.com>
2015-03-21 14:13:14 -0700

    envcov.sh: Lean toward Google shell style guide

--- envcov.sh
+++ envcov.sh
@@ -3,17 +3,17 @@
 #   XcodeCoverage by Jon Reid, http://qualitycoding/about/
 #   Copyright 2014 Jonathan M. Reid. See LICENSE.txt
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source "${DIR}/env.sh"
+scripts="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "${scripts}/env.sh"
 
 # Change the report name if you like:
 LCOV_INFO=Coverage.info
 
 XCODECOVERAGE_PATH="${SRCROOT}/XcodeCoverage"
-LCOV_PATH="${DIR}/lcov-1.11/bin"
+LCOV_PATH="${scripts}/lcov-1.11/bin"
 OBJ_DIR="${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}"
 
 # Fix for the new LLVM-COV that requires gcov to have a -v paramter
 LCOV() {
-	"${LCOV_PATH}/lcov" "$@" --gcov-tool "${DIR}/llvm-cov-wrapper.sh"
+	"${LCOV_PATH}/lcov" "$@" --gcov-tool "${scripts}/llvm-cov-wrapper.sh"
 }
