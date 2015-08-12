2ebd2d8b56cc4d4779eff324fdecdfa12cada938
Ellen Shapiro <designatednerd@gmail.com>
2015-08-12 13:28:15 -0500

    Grep the new variable

--- exportenv.sh
+++ exportenv.sh
@@ -2,4 +2,4 @@
 #   Copyright 2015 Jonathan M. Reid. See LICENSE.txt
 
 scripts="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export | egrep '( BUILT_PRODUCTS_DIR)|(CURRENT_ARCH)|(OBJECT_FILE_DIR_normal)|(SRCROOT)|(OBJROOT)' > "${scripts}/env.sh"
+export | egrep '( BUILT_PRODUCTS_DIR)|(CURRENT_ARCH)|(OBJECT_FILE_DIR_normal)|(SRCROOT)|(OBJROOT)|(XCODECOV_IGNORE)' > "${scripts}/env.sh"
