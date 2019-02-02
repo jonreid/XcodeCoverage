408dbc4e855546c2b872758a69243897521ed862
Jon Reid <jon@qualitycoding.org>
2019-02-01 16:04:16 -0800

    Bump copyright to 2019

--- envcov.sh
+++ envcov.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#   XcodeCoverage by Jon Reid, http://qualitycoding/about/
-#   Copyright 2015 Jonathan M. Reid. See LICENSE.txt
+#   XcodeCoverage by Jon Reid, https://qualitycoding.org/
+#   Copyright 2019 Jonathan M. Reid. See LICENSE.txt
 
 scripts="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${scripts}/env.sh"
--- llvm-cov-wrapper.sh
+++ llvm-cov-wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#   XcodeCoverage by Jon Reid, http://qualitycoding/about/
-#   Copyright 2015 Jonathan M. Reid. See LICENSE.txt
+#   XcodeCoverage by Jon Reid, https://qualitycoding.org/
+#   Copyright 2019 Jonathan M. Reid. See LICENSE.txt
 
 if [ "$1" = "-v" ]; then
   echo "llvm-cov-wrapper 4.2.1"
